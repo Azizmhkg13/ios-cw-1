44433d6a4911522e9f3e2b8f20a7aef6ad99b43c
Azizmhkg13 <100690892+Azizmhkg13@users.noreply.github.com>
2022-03-02 19:45:18 +0300

    i wrote my name

---
+++ azizmhkg13/azizmhkg13/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  azizmhkg13
+//
+//  Created by Abdulaziz ALGhurair on 3/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, this is aziz!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ azizmhkg13/azizmhkg13/azizmhkg13App.swift
@@ -0,0 +1,17 @@
+//
+//  azizmhkg13App.swift
+//  azizmhkg13
+//
+//  Created by Abdulaziz ALGhurair on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct azizmhkg13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
